e2463667c0394dfaf4bb8afce36b480342b4e909
Varlog Err <varlog.err@gmail.com>
2022-07-24 14:44:03 +0300

    rename @.sh file

--- @.bash
+++ @.bash
@@ -3,6 +3,7 @@ __iife() {
 
   local curdir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
   PATH="${curdir}/bin:${PATH}"
+  PATH="${curdir}/vendor/.bin:${PATH}"
   ENVAR_NAME=preprod
 
   export TESTDIR="${curdir}/tests"
